41df5e1764434152aee276fa24bf6bbd98284552
Nazarii171717 <nazarzavgar@gmail.com>
2025-11-10 00:56:02 +0100

    final

--- src/main/java/se2526/HRCalculator.java
+++ src/main/java/se2526/HRCalculator.java
@@ -9,7 +9,18 @@ public class HRCalculator {
         }
         return 220- age;
     }
-    public static String getWorkoutZone(int age, int bpm){
-        return "?";
+    public static String getWorkoutZone(int age, int bpm) {
+        int maxHR = calculateMaxHR(age);
+
+        double percent = bpm * 100.0 / maxHR;
+
+        if(percent < 50) return "Below warm-up";
+        else if(percent < 60) return "Warm-up";
+        else if(percent < 70) return "Fat burn";
+        else if(percent < 80) return "Aerobic";
+        else if(percent < 90) return "Anaerobic";
+        else if(percent <= 100) return "Red line";
+        else return "Above max HR";
     }
+
 }
--- src/test/java/HRCalculatorTest.java
+++ src/test/java/HRCalculatorTest.java
@@ -21,4 +21,16 @@ public class HRCalculatorTest {
         assertThrows(IllegalArgumentException.class,
                 ()-> HRCalculator.calculateMaxHR(-1));
     }
+
+    @Test
+    public void testGetWorkoutZone() {
+        assertEquals("Warm-up", HRCalculator.getWorkoutZone(30, 100));
+        assertEquals("Fat burn", HRCalculator.getWorkoutZone(30, 120));
+        assertEquals("Aerobic", HRCalculator.getWorkoutZone(30, 140));
+        assertEquals("Anaerobic", HRCalculator.getWorkoutZone(30, 160));
+        assertEquals("Red line", HRCalculator.getWorkoutZone(30, 175));
+        assertEquals("Above max HR", HRCalculator.getWorkoutZone(30, 250));
+        assertEquals("Below warm-up", HRCalculator.getWorkoutZone(30, 50));
+    }
+
 }
